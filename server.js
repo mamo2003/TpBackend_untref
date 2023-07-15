85a460c47ffe75faa7d76c4444aa01eeb7463f3b
mamo2003 <mlarraniaga1978@gmail.com>
2023-07-15 14:58:36 -0300

    error en ejs

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 const express = require('express');
 const dotenv= require('dotenv').config();
-const ejs =require('ejs');
 const path = require('path');
+const ejs = require ('ejs');
 const { ErrorPage } = require("./controllers/ErrorPAge");
 const { HomePage } = require('./controllers/HomePage');
 const routes = require('./routes');
 const app= express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(express.json()); 
-
 app.use((req, res, next) => {
   res.header('Content-Type', 'application/json; charset=utf-8');
   next();
 });   
-
 app.use("/api", routes);
 
 app.set("view engine", "ejs");
-app.set('views',__dirname +'/views');
 
-app.use(express.static(path.join(__dirname + '/public')));
+/* app.use(express.static(path.join(__dirname , "/public"))); */
 
 app.get('/', HomePage);
 
